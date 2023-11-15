8c844549f2550b25807f3903c0b05c8ce26a839d
Rashnotech <rashnotech@gmail.com>
2023-11-16 00:33:51 +0100

    Fixed: added each class to db_storage

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """a module that contain user attributes"""
 from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String, BinaryExpression, ForeignKey, Integer, Table
+from sqlalchemy import Column, String, Boolean, ForeignKey, Integer, Table
 from hashlib import md5
 
 
@@ -17,7 +17,7 @@ class User(BaseModel, Base):
     lastname = Column(String(128), nullable=False)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    verify = Column(BinaryExpression, nullable=True)
+    verify = Column(Boolean, nullable=True)
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
