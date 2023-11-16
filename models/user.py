880b3795c47bfe3d0c773b3bbb9fed96444d4cd8
Rashnotech <rashnotech@gmail.com>
2023-11-16 05:49:12 +0100

    Fixed: models subscriber relationship

--- models/user.py
+++ models/user.py
@@ -6,8 +6,8 @@ from hashlib import md5
 
 
 subscriber = Table('subscriber', Base.metadata,
-                   Column('following', Integer, ForeignKey('user_id', onupdate='CASCADE', ondelete='CASCADE')),
-                   Column('user_id', Integer, ForeignKey('user_id', onupdate='CASCADE', ondelete='CASCADE'), primary_key=True)
+                   Column('following', Integer, ForeignKey('users.id', onupdate='CASCADE', ondelete='CASCADE')),
+                   Column('user_id', Integer, ForeignKey('users.id', onupdate='CASCADE', ondelete='CASCADE'), primary_key=True)
                    )
 
 class User(BaseModel, Base):
@@ -17,7 +17,7 @@ class User(BaseModel, Base):
     lastname = Column(String(128), nullable=False)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    verify = Column(Boolean, nullable=True)
+    verify = Column(Boolean, nullable=False, default=False)
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
