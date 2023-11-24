43933739ea29fdc25fabeeae7bcab78d4ab8a672
Rashnotech <rashnotech@gmail.com>
2023-11-24 03:42:52 +0100

    Fixed error

--- views/web_pense/src/pages/blogs/AuthLoader.jsx
+++ views/web_pense/src/pages/blogs/AuthLoader.jsx
@@ -2,7 +2,7 @@ import { redirect } from "react-router-dom";
 
 export async function AuthLoader () {
     const session = sessionStorage.getItem('Browser_session')
-    const sess = JSON.parse(session)
+    const sess = session && JSON.parse(session)
     if (!(sess && sess.isLoggin)) throw redirect('/login?message="You must be logged in."')
     return null
 }
