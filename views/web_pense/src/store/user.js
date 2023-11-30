0118d78f4fe7380ef4088a1a1a86ae3f3ab2ced7
Abdulrasheed Aliyu <71992320+Rashnotech@users.noreply.github.com>
2023-11-30 13:44:56 +0100

    Update user.js

--- views/web_pense/src/store/user.js
+++ views/web_pense/src/store/user.js
@@ -3,7 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 const users = sessionStorage.getItem('Browser_session') ||
                 localStorage.getItem('Browser_session')
 const user_id = users && JSON.parse(users).userid
-const url = `http://127.0.0.1:5000/api/v1/user/${user_id}`
+const url = `https://pense-service.onrender.com/api/v1/user/${user_id}`
 
 const initialState = {
     status: 'idle',
