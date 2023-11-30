112f6fb356ca3ae0e0223bdb7fe53baaa4c47156
Abdulrasheed Aliyu <71992320+Rashnotech@users.noreply.github.com>
2023-11-30 13:37:59 +0100

    Imported NavLink in nav component

--- views/web_pense/src/components/Nav.jsx
+++ views/web_pense/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export default function Navbar() {
     const [state, setState] = useState(false)
