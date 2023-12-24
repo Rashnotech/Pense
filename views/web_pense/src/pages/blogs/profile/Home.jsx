3bb80487f417a3d3a4018e35e65d975de67cb121
Rashnotech <rashnotech@gmail.com>
2023-12-24 23:35:29 +0100

    Menu added

--- views/web_pense/src/pages/blogs/profile/Home.jsx
+++ views/web_pense/src/pages/blogs/profile/Home.jsx
@@ -27,29 +27,53 @@ export default function MyPost () {
                     </p>
                 </div>
             </div>
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between pr-8">
                 <p className="text-slate-500 text-sm">{post.read_time} min read</p>
                 <Menu>
-                    <Menu.Button className="text-black relative">...</Menu.Button>
-                    <Menu.Items className='flex flex-col absolute right-0 border text-sm bg-slate-50 p-2'>
+                    <Menu.Button className="inline-flex justify-center rounded-md px-4 py-2 text-xl text-gray-700 font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">...</Menu.Button>
+                    <Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none'>
                         <Menu.Item>
-                            {({ active }) => (
-                                <a
-                                className={`${active && 'bg-blue-500'}`}
-                                href="/account-settings"
-                                >
-                                    Edit story
-                                </a>
+                        {({ active }) => (
+                            <button
+                                className={`${
+                                active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                            >
+                                {active ? (
+                                <EditActiveIcon
+                                    className="mr-2 h-5 w-5"
+                                    aria-hidden="true"
+                                />
+                                ) : (
+                                <EditInactiveIcon
+                                    className="mr-2 h-5 w-5"
+                                    aria-hidden="true"
+                                />
+                                )}
+                                Edit story
+                            </button>
                             )}
                         </Menu.Item>
                         <Menu.Item>
                             {({ active }) => (
-                                <a
-                                className={`${active && 'bg-blue-500'}`}
-                                href="/account-settings"
-                                >
-                                    Delete story
-                                </a>
+                            <button
+                                className={`${
+                                active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                            >
+                                {active ? (
+                                <DeleteActiveIcon
+                                    className="mr-2 h-5 w-5 text-blue-400"
+                                    aria-hidden="true"
+                                />
+                                ) : (
+                                <DeleteInactiveIcon
+                                    className="mr-2 h-5 w-5 text-blue-400"
+                                    aria-hidden="true"
+                                />
+                                )}
+                                Delete
+                            </button>
                             )}
                         </Menu.Item>
                     </Menu.Items>
@@ -58,3 +82,87 @@ export default function MyPost () {
          </article>) : <p className="text-gray-700 text-base">You have no post</p>
     )
 }
+
+
+function EditInactiveIcon(props) {
+    return (
+      <svg
+        {...props}
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M4 13V16H7L16 7L13 4L4 13Z"
+          fill="#EDE9FE"
+          stroke="#A78BFA"
+          strokeWidth="2"
+        />
+      </svg>
+    )
+  }
+  
+  function EditActiveIcon(props) {
+    return (
+      <svg
+        {...props}
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M4 13V16H7L16 7L13 4L4 13Z"
+          fill="#8B5CF6"
+          stroke="#C4B5FD"
+          strokeWidth="2"
+        />
+      </svg>
+    )
+  }
+
+  function DeleteInactiveIcon(props) {
+    return (
+      <svg
+        {...props}
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="5"
+          y="6"
+          width="10"
+          height="10"
+          fill="#EDE9FE"
+          stroke="#A78BFA"
+          strokeWidth="2"
+        />
+        <path d="M3 6H17" stroke="#A78BFA" strokeWidth="2" />
+        <path d="M8 6V4H12V6" stroke="#A78BFA" strokeWidth="2" />
+      </svg>
+    )
+  }
+  
+  function DeleteActiveIcon(props) {
+    return (
+      <svg
+        {...props}
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="5"
+          y="6"
+          width="10"
+          height="10"
+          fill="#8B5CF6"
+          stroke="#C4B5FD"
+          strokeWidth="2"
+        />
+        <path d="M3 6H17" stroke="#C4B5FD" strokeWidth="2" />
+        <path d="M8 6V4H12V6" stroke="#C4B5FD" strokeWidth="2" />
+      </svg>
+    )
+  }
+  
